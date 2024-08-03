cfe2237b07c3b60491266ff83502ea48d928ed0c
BaconStorage <r85hjdvvm6aq96u@gmail.com>
2024-08-03 02:19:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '#!')
+
+@client.event
+async def on_ready():
+    print(f'Successfully logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+
+
+# Commands
+
+@client.command()
+async def hello(ctx):
+    await ctx.channel.send(f'Hello! {ctx.author}')
+
+
+
+token = os.environ['TOKEN']
+client.run(token)
