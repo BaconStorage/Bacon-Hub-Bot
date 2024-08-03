848abfa44f01f7eda1b3924e3918c6f502ba070e
BaconStorage <r85hjdvvm6aq96u@gmail.com>
2024-08-03 02:38:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import os
 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '#!')
 
-@client.event
+@commands.Bot(command_prefix = '#!').event
 async def on_ready():
     print(f'Successfully logged in as {client.user}')
 
 
-@client.event
+@client = commands.Bot(command_prefix = '#!')
+.event
 async def on_message(message):
     await client.process_commands(message)
 
 
 # Commands
 
-@client.command()
+@client = commands.Bot(command_prefix = '#!')
+.command()
 async def hello(ctx):
     await ctx.channel.send(f'Hello! {ctx.author}')
 
