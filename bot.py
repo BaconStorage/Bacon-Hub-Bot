55afc636bf205c0cc0a23e691e6fca717d845ac1
BaconStorage <BaconStorage@hotmail.com>
2025-02-16 02:08:30 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,24 +16,20 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-# Command: Buy
 @bot.command(name='Buy')
 async def buy(ctx):
     await ctx.send('Thank you for your interest in buying! Please visit our website to make a purchase.')
 
-# Command: Getscript
 @bot.command(name='Getscript')
 async def getscript(ctx):
     await ctx.send('Here is the script you requested: [link to script]')
 
-# Command: GiveRole
 @bot.command(name='GiveRole')
 @commands.has_permissions(administrator=True)
 async def giverole(ctx, member: discord.Member, role: discord.Role):
     await member.add_roles(role)
     await ctx.send(f'Role {role.name} has been given to {member.mention}')
 
-# Command: BuyerRole
 @bot.command(name='BuyerRole')
 async def buyerrole(ctx):
     role = discord.utils.get(ctx.guild.roles, name='Buyer')
@@ -43,9 +39,20 @@ async def buyerrole(ctx):
     else:
         await ctx.send('Role "Buyer" does not exist.')
 
-# Command: Howtobuy
 @bot.command(name='Howtobuy')
 async def howtobuy(ctx):
     await ctx.send('To buy, follow these steps:\n1. Visit our website.\n2. Select the product you want to purchase.\n3. Follow the checkout process.\n4. Contact our support if you need assistance.')
+
+@bot.command(name='Commands')
+async def commands(ctx):
+    commands_list = """
+    Here are the available commands:
+    - !Buy: Information on how to buy
+    - !Getscript: Get the script link
+    - !GiveRole: Give a role to a member (Admin only)
+    - !BuyerRole: Assign the "Buyer" role to yourself
+    - !Howtobuy: Guide on how to buy
+    """
+    await ctx.send(commands_list)
 
 bot.run(DISCORD_TOKEN)
